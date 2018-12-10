65f0f2c90179b5b3d2a0fa1694fdc12cfb1371d2
zjiejie <45325767+zjiejie@users.noreply.github.com>
2018-12-10 20:10:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,21 +20,21 @@ var server = http.createServer(function(request, response){
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('HTTP 路径为\n' + path)
-  if(path == '/style.js'){
+  if(path == '/style.css'){
     response.setHeader('Content-Type', 'text/css; charset=utf-8')
     response.write('body{background-color: #ddd;}h1{color: red;}')
     response.end()
-  }else if(path == '/script.html'){
+  }else if(path == '/script.js'){
     response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
     response.write('alert("这是JS执行的")')
     response.end()
-  }else if(path == '/index.css'){
+  }else if(path == '/index.html'){
     response.setHeader('Content-Type', 'text/html; charset=utf-8')
     response.write('<!DOCTYPE>\n<html>'  + 
-      '<head><link rel="stylesheet" href="/style.js">' +
+      '<head><link rel="stylesheet" href="/style.css">' +
       '</head><body>'  +
       '<h1>你好</h1>' +
-      '<script src="/script.html"></script>' +
+      '<script src="/script.js"></script>' +
       '</body></html>')
     response.end()
   }else{
